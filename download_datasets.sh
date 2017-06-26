b7812edc7d88227e8a87214eba20546a4b42bc9c
leehomyc <leehomyc@gmail.com>
2017-06-26 16:27:25 -0400

    fix

--- download_datasets.sh
+++ download_datasets.sh
@@ -1,8 +1,9 @@
 FILE=horse2zebra
 URL=https://people.eecs.berkeley.edu/~taesung_park/CycleGAN/datasets/$FILE.zip
-ZIP_FILE=./input/$FILE.zip
-TARGET_DIR=./input/$FILE/
+mkdir ../input
+ZIP_FILE=../input/$FILE.zip
+TARGET_DIR=../input/$FILE/
 wget -N $URL -O $ZIP_FILE
 mkdir $TARGET_DIR
-unzip $ZIP_FILE -d ./input/
+unzip $ZIP_FILE -d ../input/
 rm $ZIP_FILE
